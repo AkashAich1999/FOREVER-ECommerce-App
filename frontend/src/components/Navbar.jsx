9532a909fdc1b4fe7124eabcefcf288d455a2e66
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2025-11-28 15:44:59 +0530

    Product Page Completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { ShopContext } from "../context/ShopContext";
 
 const Navbar = () => {
     const [visible, setVisible] = useState(false);
-    const { showSearch, setShowSearch } = useContext(ShopContext);
+    const { showSearch, setShowSearch, getCartCount } = useContext(ShopContext);
 
   return (
     <div className="py-5 font-medium flex justify-between items-center">
@@ -48,7 +48,9 @@ const Navbar = () => {
 
             <Link to='/cart' className="relative">
                 <img src={assets.cart_icon} alt="" className="w-5 min-w-5" />
-                <p className="absolute right-[-5px] bottom-[-5px] bg-black text-white leading-4 w-4 text-center aspect-square rounded-full text-[8px]">10</p>
+                <p className="absolute right-[-5px] bottom-[-5px] bg-black text-white leading-4 w-4 text-center aspect-square rounded-full text-[8px]">
+                    { getCartCount() }
+                </p>
             </Link>
             <img onClick={() => setVisible(true)} src={assets.menu_icon} alt="" className="w-5 cursor-pointer sm:hidden" />
         </div>
--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,17 +1,59 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+import { toast } from "react-toastify";
 
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
-    const currency = '$';
+    const currency = '₹';
     const deliveryfee = 10;
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = async (itemId, size) => {
+        if(!size) {
+            toast.error("Select Product Size");
+            return;
+        }
+
+        let cartData = structuredClone(cartItems);  // deep copy of cartItems object
+
+        if (cartData[itemId]) { // If Product Already Exists
+            if (cartData[itemId][size]) {  // If Size Also Exists → Increase Quantity
+                cartData[itemId][size] += 1;
+            } else {
+                cartData[itemId][size] = 1; // If New Size → Add Size
+            }
+        } else {   // If Product Does NOT Exist Yet 
+            cartData[itemId] = {};
+            cartData[itemId][size] = 1; 
+        }
+
+        setCartItems(cartData);
+    } 
+
+    useEffect(() => {
+        console.log(cartItems);
+    }, [cartItems]);
+
+    const getCartCount = () => {
+        let totalCount = 0;
+
+        for (const productId in cartItems) {
+            for (const size in cartItems[productId]) {
+                totalCount += cartItems[productId][size];
+            }
+        }
+
+        return totalCount;
+    };
 
     const value = {
         products, currency, deliveryfee,
-        search, setSearch, showSearch, setShowSearch
+        search, setSearch, showSearch, setShowSearch,
+        cartItems, addToCart,
+        getCartCount
     }
 
     return (
