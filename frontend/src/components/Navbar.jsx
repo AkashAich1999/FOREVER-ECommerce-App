86edc19f3814e28f6662e5e0d4f603fc49349aa7
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2026-01-17 21:09:12 +0530

    Login Page Created

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
             <img onClick={() => setShowSearch(!showSearch)} src={assets.search_icon} alt="" className="w-5 cursor-pointer" />
             
             <div className="group relative">
-                <img src={assets.profile_icon} alt="" className="w-5 cursor-pointer" />
+                <Link to={"/login"}><img src={assets.profile_icon} alt="" className="w-5 cursor-pointer" /></Link>
                 <div className="absolute hidden dropdown-menu right-0 pt-4 group-hover:block">
                     <div className="flex flex-col w-36 py-3 px-5 gap-2 bg-slate-100 text-gray-500 rounded">
                         <p className="cursor-pointer hover:text-black">My Profile</p>
