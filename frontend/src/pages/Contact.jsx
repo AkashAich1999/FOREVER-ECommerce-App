19ee12c4d5ac08bd7a6364a4b4b14c1474feb4d1
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2026-01-18 20:19:17 +0530

    Created Contact Us Page

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,6 +1,33 @@
+import { assets } from "../assets/assets";
+import NewsletterBox from "../components/NewsletterBox";
+import Title from "../components/Title";
+
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      
+      <div className="border-t pt-10 text-2xl text-center">
+        <Title text1={"CONTACT"} text2={"US"} />
+      </div>
+
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
+
+        <img className="w-full md:max-w-[450px]" src={assets.contact_img} alt="" />
+
+        <div className="flex flex-col gap-6 justify-center items-start">
+          <p className="text-xl font-semibold text-gray-600">Our Store</p>
+          <p className="text-gray-500">54709 Willms Station <br /> Suite 350, Washington, USA</p>
+          <p className=" text-gray-500">Tel: (415) 555-0132 <br /> Email: greatstackdev@gmail.com</p>
+          <p className="text-xl font-semibold text-gray-600">Careers at Forever</p>
+          <p className="text-gray-500">Learn more about our teams and job openings.</p>
+          <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">Explore Jobs</button>
+        </div>
+
+      </div>
+
+      <NewsletterBox />
+
+    </div>
   )
 }
 
